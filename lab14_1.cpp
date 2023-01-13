d68982d93f59c17e1fc6db6e5376c55117df8cb9
Piicharpa <neuah2515@gmail.com>
2023-01-13 16:56:34 +0700

    com 1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    
+    double summ;
+    for(int i=0;i<N;i++){
+        summ += A[i];
+        B[0] = summ / N;
+    } 
+  
+    double sum;
+    for(int i=0;i<N;i++){
+        sum += pow(A[i]-B[0],2);
+        B[1] = pow(sum/N,0.5);
+    }
+    
+    double geo =1;
+    for(int i=0;i<N;i++){
+        geo *= A[i];
+        B[2] = pow(geo,(1.00/N));
+    }
+
+    double har;
+    for(int i=0;i<N;i++){
+        har += 1/A[i]; 
+        B[3] = N / har;
+    }
+    
+    double max = A[0],min = A[0];
+    for(int i=1;i<N;i++){
+        if(A[i]>max)  max=A[i];
+        B[4] = max;
+        
+        if(A[i]<min)  min=A[i];
+        B[5] = min;
+    }
+     
+    
+}
